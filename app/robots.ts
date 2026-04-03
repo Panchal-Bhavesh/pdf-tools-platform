5c0e41c6246106ce9a5eb20cd29cd8495c6b0c84
Bhavesh Panchal <bhavesh.panchal@bytestechnolab.com>
2026-04-04 00:21:33 +0530

    Fix: Robots File Issue

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   const siteUrl =
-    process.env.NEXT_PUBLIC_SITE_URL || "https://pagelypdf.com";
+    process.env.NEXT_PUBLIC_SITE_URL || "https://pagelypdf.vercel.app";
   return {
     rules: [
       {
